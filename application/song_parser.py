db10101369da17cbcefb2020a38de9671e652ea6
Damien <damien@dam.io>
2014-02-22 13:50:29 -0500

    Working .wav generation

--- application/song_parser.py
+++ application/song_parser.py
@@ -3,7 +3,7 @@ import re
 def parse(song):
     result = []
     for match in re.findall(r"[a-gA-G ][#*]*\d*",song):
-        match = match.replace(' ','r')
+        match = match.replace(' ','r').lower()
         if len(match) == 1:
             result.append((match,1))
         elif len(match) == 3:
@@ -15,3 +15,10 @@ def parse(song):
     return result
 
 #print parse("A4BCD3F*3 3F3DEFC#3FF")
+
+if __name__ == "__main__":
+    song = parse("A4BCD3F*3 3F3DEFC#3FF")
+    print song
+    
+    import pysynth
+    pysynth.make_wav(song)
